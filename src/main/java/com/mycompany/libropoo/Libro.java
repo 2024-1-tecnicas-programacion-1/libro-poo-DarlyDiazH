336e75c28893903e77c8f2e2945264d1c9e71883
david cardenas <david.cardenas0718@gmail.com>
2024-04-22 08:58:14 -0500

    Agregar Código Libro Y Principal
    Se agregó método main, constructor, método mostrarInformacion, marcarLeido, isLeido, esAntiguo.

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -2,4 +2,51 @@ package com.mycompany.libropoo;
 
 public class Libro {
     
+    //ATRIBUTO
+    
+    private String titulo;
+    private String autor;
+    private int anioPublicacion;
+    private String genero;
+    private boolean leido;
+    
+    //MÉTODOS
+    
+    //Complejidad temporal: O(1) Tiempo Constante
+    
+    public Libro(String titulo, String autor, int anioPublicacion, String genero, boolean leido){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anioPublicacion = anioPublicacion;
+        this.genero = genero;
+        this.leido = leido;
+    }
+    
+    //Complejidad temporal: O(1) Tiempo Constante
+    
+    public String mostrarInformacion(){
+        return ("Título: "+titulo+", Autor: "+autor+", Año de publicación: "+anioPublicacion+", Género: "+genero);
+    }
+    
+    //Complejidad temporal: O(1) Tiempo Constante
+    
+    public void marcarLeido(){
+        leido = true;
+    }
+    
+    //Complejidad temporal: O(1) Tiempo Constante
+    
+    public boolean isLeido() {
+        return leido;
+    }
+    
+    //Complejidad temporal: O(1) Tiempo Constante
+    
+    public boolean esAntiguo(){
+        if (anioPublicacion<1974) {
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,41 @@
 package com.mycompany.libropoo;
 
+import java.util.Scanner;
+
 public class Principal {
     
+    //Complejidad temporal: O(1) Tiempo constante
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Titulo: ");
+        String titulo=sc.nextLine();
+        System.out.print("Autor: ");
+        String autor=sc.nextLine();
+        System.out.print("Genero: ");
+        String genero=sc.nextLine();
+        System.out.print("Año de Publicacion: ");
+        int anioPublicacion=sc.nextInt();
+        boolean leido = false;
+
+        Libro nuevoLibro = new Libro(titulo, autor, anioPublicacion, genero, leido);
+        System.out.println(nuevoLibro.mostrarInformacion());
+        
+        nuevoLibro.marcarLeido();
+        
+        if (nuevoLibro.isLeido() == true) {
+            System.out.println("Ya leiste este libro");
+        }else{
+            System.out.println("Aun no leiste este libro");
+        }
+        
+        if (nuevoLibro.esAntiguo() == true) {
+            System.out.println("Este libro es Antiguo");
+        }else{
+            System.out.println("Este libro es Nuevo");
+        }
+        
+    }
 }
+    
